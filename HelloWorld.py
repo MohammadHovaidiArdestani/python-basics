ee339fb2091fb3e4500227db7da6fca2e81de80c
MohammadHovaidiArdestani <M.Hovaidi-Ardestani@hotmail.com>
2022-01-27 11:50:35 +0100

    adding HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 10:49:51 2022
+
+@author: mohammadhovaidi-ardestani
+
+PCEP - Certified Entry-Level Python Programmer 
+PCAP: Certified Associate in Python Programming certification
+"""
+
+print("hissssss......")
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("I'm")
+print("\"learning\"")
+print("\"\"Python\"\"")
+
+# Operators: remainder (modulo)
+print(14 % 4)
+print(14//4)
+print(12 % 4.5)
