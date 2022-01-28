64519d015393aaa4392084f393c8653ac45dcfd1
MohammadHovaidiArdestani <M.Hovaidi-Ardestani@hotmail.com>
2022-01-28 14:26:13 +0100

    adding condition examples in  Module3.py

--- Module3.py
+++ Module3.py
@@ -7,3 +7,20 @@ Created on Fri Jan 28 14:09:24 2022
 https://edube.org/learn/pe-1/
 """
 
+x = int(input("x: "))
+if x >= 100:
+    print("True")
+else:
+    print("False")
+    
+# Conditions and conditional execution
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2: larger_number = number1
+else: larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
