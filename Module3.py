c17af9bf7873d286ce378ee89fe21fa5ae6e341e
MohammadHovaidiArdestani <M.Hovaidi-Ardestani@hotmail.com>
2022-01-30 13:43:34 +0100

    The continue statement - the Pretty Vowel Eater

--- Module3.py
+++ Module3.py
@@ -7,21 +7,13 @@ Created on Fri Jan 28 14:09:24 2022
 https://edube.org/learn/pe-1/
 """
 
-<<<<<<< HEAD
-x = int(input("x: "))
-if x >= 100:
-    print("True")
-else:
-    print("False")
-=======
 """ x = int(input("x: "))
 if x >= 100:
     print("True")
 else:
-    print("False") """
->>>>>>> PythonCodes
-    
-# Conditions and conditional execution
+    print("False")
+     """
+""" # Conditions and conditional execution
 # Read two numbers
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
@@ -31,4 +23,30 @@ if number1 > number2: larger_number = number1
 else: larger_number = number2
 
 # Print the result
-print("The larger number is:", larger_number)
+print("The larger number is:", larger_number) """
+
+
+# The continue statement - the Pretty Vowel Eater
+word_without_vowels = ""
+user_word = input("Word: ")
+user_word = user_word.upper()
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+
+
+for letter in user_word:
+    # Complete the body of the loop.
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "U":
+        continue
+    word_without_vowels += letter
+print(word_without_vowels)
+
+# Print the word assigned to word_without_vowels.
